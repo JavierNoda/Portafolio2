b75c7dedc9d23dc4e45007e9c91a3a6ae64e6e70
JavierNoda <157540887+JavierNoda@users.noreply.github.com>
2024-01-23 18:03:12 -0400

    Initial commit

---
+++ Portafolio2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portafolio2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
